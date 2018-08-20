ca007ab4dca05a9f5189017cc323cb69a9958a8b
kamil25 <kamilgnadczynski@gmail.com>
2018-08-20 14:09:05 +0200

    update the UI and linked backend

--- onlineshopping/src/main/java/com/kmg/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/kmg/onlineshopping/controller/PageController.java
@@ -2,11 +2,13 @@ package com.kmg.onlineshopping.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kmg.shoppingbackend.dao.CategoryDAO;
+import com.kmg.shoppingbackend.dto.Category;
 
 @Controller
 public class PageController {
@@ -43,6 +45,37 @@ public class PageController {
 		mv.addObject("userClickContact", true);
 		return mv;
 	}
+	
+	//methods to load all the products
+	
+	@RequestMapping(value = "/show/all/products")
+	public ModelAndView showAllProducts() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "All products");
+		
+		//passing the list of categories
+		mv.addObject("categories", categoryDAO.list());
+		
+		mv.addObject("userClickAllProducts", true);
+		return mv;
+	}
+	
+	@RequestMapping(value = "/show/category/{id}/products")
+	public ModelAndView showCategoryProducts(@PathVariable("id") int id) {
+		ModelAndView mv = new ModelAndView("page");
+		
+		Category category = null;
+		category = categoryDAO.get(id);
+		
+		mv.addObject("title", category.getName());
+		
+		//passing the list of categories
+		mv.addObject("categories", categoryDAO.list());
+		mv.addObject("category", category);
 
+		
+		mv.addObject("userClickCategoryProducts", true);
+		return mv;
+	}
 }
   
