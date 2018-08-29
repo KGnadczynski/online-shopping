edc340bbd24161c222c74372f9898e206ccdef6b
kamil25 <kamilgnadczynski@gmail.com>
2018-08-29 20:54:26 +0200

    adding new category

--- onlineshopping/src/main/java/com/kmg/onlineshopping/controller/ManagementController.java
+++ onlineshopping/src/main/java/com/kmg/onlineshopping/controller/ManagementController.java
@@ -12,9 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kmg.onlineshopping.util.FileUploadUtility;
@@ -56,17 +58,42 @@ public class ManagementController {
 			if(operation.equals("product")) {
 				mv.addObject("message", "Product Submitted Successfully!");
 			}
+			else if(operation.equals("category")) {
+				mv.addObject("message", "Category submitted successfully!");
+			}
 		}
 		
 		return mv;
 	}
 	
+	@RequestMapping(value="/{id}/product", method=RequestMethod.GET)
+	public ModelAndView showEditProduct(@PathVariable int id) {
+		
+		ModelAndView mv = new ModelAndView("page");
+		
+		mv.addObject("userClickManageProducts", true);
+		mv.addObject("title", "Manage Products");
+		Product nProduct = productDAO.get(id);
+		
+		mv.addObject("product", nProduct);
+		
+		return mv;
+	}
+	
+	
+	
 	@RequestMapping(value="/products", method=RequestMethod.POST)
 	public String handleProductSubmission(@Valid @ModelAttribute("product") Product mProduct, BindingResult results, Model model,
 			HttpServletRequest request) {
 		
-		new ProductValidator().validate(mProduct, results);
-		
+		if(mProduct.getId() == 0) {
+			new ProductValidator().validate(mProduct, results);
+		}
+		else {
+			if(!mProduct.getFile().getOriginalFilename().equals("")) {
+				new ProductValidator().validate(mProduct, results);
+			}
+		}
 		if(results.hasErrors()) {
 			
 			model.addAttribute("userClickManageProducts", true);
@@ -78,7 +105,12 @@ public class ManagementController {
 		
 		logger.info(mProduct.toString());
 		
+		if(mProduct.getId() == 0) {
 		productDAO.add(mProduct);
+		}
+		else {
+			productDAO.update(mProduct);
+		}
 		
 		if(!mProduct.getFile().getOriginalFilename().equals("")) {
 			FileUploadUtility.uploadFile(request, mProduct.getFile(), mProduct.getCode());
@@ -89,12 +121,39 @@ public class ManagementController {
 		return "redirect:/manage/products?operation=product";
 	}
 	
+	@RequestMapping(value = "/product/{id}/activation", method=RequestMethod.POST)
+	@ResponseBody
+	public String handleProductActivation(@PathVariable int id) {
+		Product product = productDAO.get(id);
+		boolean isActive = product.isActive();
+		
+		product.setActive(!product.isActive());
+		
+		productDAO.update(product);
+		
+		return (isActive)? "You have succesfully deactivated the product with id " + product.getId() 
+						: "You have succesfully activated the product with id " + product.getId();
+	}
+	
+	@RequestMapping(value="/category", method=RequestMethod.POST)
+	public String handleCategorySubmisssion(@ModelAttribute Category category) {
+		
+		categoryDAO.add(category);
+		
+		return "redirect:/manage/products?operation=category";
+		
+	}
 	
 	@ModelAttribute("categories")
 	public List<Category> getCategories(){
 		
 		return categoryDAO.list();
 		
+	}
+	
+	@ModelAttribute("category")
+	public Category getCategory() {
+		return new Category();
 	}
 
 }
--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -1,8 +1,8 @@
-$(function(){
-	
-	switch(menu) {
-	
-	
+$(function() {
+
+	switch (menu) {
+
+
 	case 'Contact us':
 		$('#contact').addClass('active');
 		break;
@@ -14,137 +14,260 @@ $(function(){
 		break;
 	case 'Manage Products':
 		$('#manageProducts').addClass('active');
-		break;	
+		break;
 	default:
-		if(menu == "Home") break;
+		if (menu == "Home") break;
 		$('#listProducts').addClass('active');
-		$('#a_'+menu).addClass('active');
-	break;
+		$('#a_' + menu).addClass('active');
+		break;
 	}
-		
-	
-	
-	
+
+
+
+
 	var $table = $('#productListTable');
-	
-	if($table.length) {
+
+	if ($table.length) {
 		//console.log('Inside the table!');
-		
+
 		var jsonUrl = '';
-		if(window.categoryId == '') {
+		if (window.categoryId == '') {
 			jsonUrl = window.contextRoot + '/json/data/all/products';
+		} else {
+			jsonUrl = window.contextRoot + '/json/data/category/' + window.categoryId + '/products';
 		}
-		else {
-			jsonUrl = window.contextRoot + '/json/data/category/'+ window.categoryId +'/products';
-		}
-		
+
 		$table.DataTable({
-			
-			lengthMenu: [[3,5,10,-1], ['3 Records', '5 Records', '10 Records', 'ALL']],
-			pageLength: 5,
-			ajax:{
-				url: jsonUrl,
-				dataSrc: ''
+			lengthMenu : [ [ 3, 5, 10, -1 ], [ '3 Records', '5 Records', '10 Records', 'ALL' ] ],
+			pageLength : 5,
+			ajax : {
+				url : jsonUrl,
+				dataSrc : ''
 			},
-			columns: [
+			columns : [
 				{
-					data: 'code',
-					mRender: function(data, type, row) {
+					data : 'code',
+					mRender : function(data, type, row) {
 						return '<img src="' + window.contextRoot
-						+ '/resources/images/' + data
-						+ '.jpg" class="dataTableImg"/>';
+							+ '/resources/images/' + data
+							+ '.jpg" class="dataTableImg"/>';
 					}
 				},
 				{
-					data: 'name'
+					data : 'name'
 				},
 				{
-					data: 'brand'
+					data : 'brand'
 				},
 				{
-					data: 'unitPrice',
-					mRender: function(data, type, row) {
+					data : 'unitPrice',
+					mRender : function(data, type, row) {
 						return '&#8377; ' + data
 					}
 				},
 				{
-					data: 'quantity',
-					mRender: function(data, type, row) {
-						if(data<1) {
+					data : 'quantity',
+					mRender : function(data, type, row) {
+						if (data < 1) {
 							return '<p>Out of stock!</p>';
 						}
 						return data;
 					}
 				},
 				{
-					data: 'id',
-					bSortable: false,
-					mRender: function(data, type, row){
-						
+					data : 'id',
+					bSortable : false,
+					mRender : function(data, type, row) {
+
 						var str = '';
-						str += '<a href="'+window.contextRoot+ '/show/'+data+'/product">View</a><br>';
-						
-						if(row.quantity<1) {
+						str += '<a href="' + window.contextRoot + '/show/' + data + '/product">View</a><br>';
+
+						if (row.quantity < 1) {
 							str += ' ';
-							
+
+						} else {
+							str += '<a href="' + window.contextRoot + '/cart/add/' + data + '/product">Add to cart</a>';
 						}
-						else {
-							str += '<a href="'+window.contextRoot+ '/cart/add/'+data+'/product">Add to cart</a>';
-						}
-						
-						
+
+
 						return str;
-						
+
 					}
 				}
-				
+
 			]
 		});
 	}
-	
+
 	var $alert = $('alert');
-	
-	if($alert.length) {
-		
-		setTimeout(function(){
+
+	if ($alert.length) {
+
+		setTimeout(function() {
 			$alert.fadeOut('slow');
 		}, 3000)
 	}
-	
+
 	$('.switch input[type="checkbox"]').on('change', function() {
-		
+
 		var checkbox = $(this);
 		var checked = checkbox.prop('checked');
-		var dMsg = (checked)? 'You want to activate the product?' :
-								'You want to deactivate the product?';
+		var dMsg = (checked) ? 'You want to activate the product?' :
+			'You want to deactivate the product?';
 		var value = checkbox.prop('value');
-		
+
 		bootbox.confirm({
-			size: 'medium',
-			title: 'Product Activation & Deactivation',
-			message: dMsg,
-			callback: function(confirmed){
-				
-				if(confirmed) {
-					
+			size : 'medium',
+			title : 'Product Activation & Deactivation',
+			message : dMsg,
+			callback : function(confirmed) {
+
+				if (confirmed) {
+
 					console.log(value);
 					bootbox.alert({
-						size: 'medium',
-						title: 'Information',
-						message: 'You are going to perform operation on product' + value
+						size : 'medium',
+						title : 'Information',
+						message : 'You are going to perform operation on product' + value
 					});
-				}
-				else{
+				} else {
 					checkbox.prop('checked', !checked);
 				}
 			}
 		});
 	});
-	
-	
-	
-	
-	
-	
-	
+
+
+	var $adminProductsTable = $('#adminProductsTable');
+
+	if ($adminProductsTable.length) {
+		//console.log('Inside the table!');
+
+		var jsonUrl = window.contextRoot + '/json/data/admin/all/products';
+		
+
+		$adminProductsTable.DataTable({
+			lengthMenu : [ [ 10, 30, 50, -1 ], [ '10 Records', '30 Records', '50 Records', 'ALL' ] ],
+			pageLength : 30,
+			ajax : {
+				url : jsonUrl,
+				dataSrc : ''
+			},
+			columns : [
+				{
+					data: 'id'
+				},
+				{
+					data : 'code',
+					bSortable: false,
+					mRender : function(data, type, row) {
+						return '<img src="' + window.contextRoot
+							+ '/resources/images/' + data
+							+ '.jpg" class="adminDataTableImg"/>';
+					}
+				},
+				{
+					data : 'name'
+				},
+				{
+					data : 'brand'
+				},
+				{
+					data : 'quantity',
+					mRender : function(data, type, row) {
+						if (data < 1) {
+							return '<p>Out of stock!</p>';
+						}
+						return data;
+					}
+				},
+				{
+					data : 'unitPrice',
+					mRender : function(data, type, row) {
+						return '&#8377; ' + data
+					}
+				},
+				{
+					data : 'active',
+					bSortable: false,
+					mRender: function(data, type, row) {
+						var str = '';
+							
+							str += '<label class="switch">';
+							if(data){
+								str += '<input type="checkbox" checked="checked" value="'+row.id+'" />';
+
+							}
+							else {
+								str += '<input type="checkbox" value="'+row.id+'" />';
+
+							}
+							str += '<div class="slider"></div></label>';
+							return str;
+										
+					}
+					
+
+					},
+					{
+						data: 'id',
+						bSortable: false,
+						mRender: function(data, type, row) {
+							var str='';
+							str += '<a href="'+window.contextRoot+'/manage/'+data+'/product" class="btn btn-warning">';
+							str += '<span class="glyphicon glyphicon-pencil"></span></a>'
+								
+								return str;
+						}
+					}
+				
+
+			],
+		initComplete: function() {
+			
+			var api = this.api();
+			api.$('.switch input[type="checkbox"]').on('change', function() {
+
+				var checkbox = $(this);
+				var checked = checkbox.prop('checked');
+				var dMsg = (checked) ? 'You want to activate the product?' :
+					'You want to deactivate the product?';
+				var value = checkbox.prop('value');
+
+				bootbox.confirm({
+					size : 'medium',
+					title : 'Product Activation & Deactivation',
+					message : dMsg,
+					callback : function(confirmed) {
+
+						if (confirmed) {
+
+							console.log(value);
+							var activationUrl = window.contextRoot + '/manage/product/' + value + '/activation';
+							
+							$.post(activationUrl, function(data){
+								bootbox.alert({
+									size : 'medium',
+									title : 'Information',
+									message: data
+								});	
+							});
+							
+							
+						} else {
+							checkbox.prop('checked', !checked);
+						}
+					}
+				});
+			});
+			
+		}
+			
+		});
+	}
+
+
+
+
+
 });
